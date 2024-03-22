53a707e29f5a8a19d1a5ce810a8fc70d6a9623e1
baz263 <61625356+baz263@users.noreply.github.com>
2024-03-22 13:42:36 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,11 @@
-import streamlit as st
 
-st.write('Welcome to Streamlit')
+import streamlit as st
+from streamlit_gsheets import GSheetsConnection
+
+# Create a connection object.
+conn = st.connection("nearest_asteroid", type=GSheetsConnection)
+
+df = conn.read()
+
+for row in df.itertuples():
+    st.write(f"{row.name} has a :{row.pet}:")
